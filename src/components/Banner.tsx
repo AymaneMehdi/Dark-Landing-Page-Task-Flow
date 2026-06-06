878f382eedddcb8f5bc1cb6b52847a38715366db
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-06 23:37:54 +0100

    Add Banner component.

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FEECFF,#FECAFF,#FEB5FF,#FFDBE7,#FFF4D6,#F3FCD7,#D2F5EE,#C3EFFF,#FBEFFF)]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -{" "}
+          </span>
+          <a href="#" className="underline underline-offset-4 font-medium">
+            Explore the demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
